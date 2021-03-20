627faf3d9b8a10664a3763b91599dffbb3734386
Cyrille Yemeli Tasse <yemelitassecyrille@gmail.com>
2021-03-20 20:25:20 +0100

    Separated visual studio solutions
    Separated the VS solution for developing the Xamarin app from the solution for developing the WebAPI.
    
    This separation is because otherwise developing and testing silmutaneously on both the api server and the mobile app is inefficient.

--- Global/Models/ApplicationUserDTO.cs
+++ Global/Models/ApplicationUserDTO.cs
@@ -4,10 +4,10 @@ namespace Global.Models
 {
     public class ApplicationUserDTO
     {
-        public string Id { get; set; }
+        public long Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool RememberMe { get; set; }
         public string PhoneNumber { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
