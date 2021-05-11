33bd851de1a96cca88f51858fcba63d04dfcb8b3
electronF <bertrandovide@gmail.com>
2021-05-11 23:39:00 +0200

    First commit: Minimal files for Flutter application

---
+++ Flutter/chatonline/android/app/src/main/kotlin/com/example/chatonline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatonline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/chatonline/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chatonline/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatOnline',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomePage(title: ''),
+    );
+  }
+}
