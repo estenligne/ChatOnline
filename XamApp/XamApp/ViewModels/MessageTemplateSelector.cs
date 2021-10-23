45adea59dd96cb06e7cc862e7dbaa5967909206a
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-10-23 17:35:25 +0100

    Implemented the UI for event-messages using DataTemplateSelector

---
+++ XamApp/XamApp/ViewModels/MessageTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace XamApp.ViewModels
+{
+    public class MessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate EventDataTemplate { get; set; }
+        public DataTemplate MessageDataTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Message message = (Message)item;
+            return message.IsAnEvent ? EventDataTemplate : MessageDataTemplate;
+        }
+    }
+}
