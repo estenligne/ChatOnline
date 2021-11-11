91794b0e8c4e3fc9177fd0fca1481c4000efc6de
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-11-11 14:29:40 +0100

    Implemented LoggerProfile.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Web;
+using WebAPI.Setup;
 using System;
 
 namespace WebAPI
@@ -10,35 +10,24 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            string loggerType = Environment.GetEnvironmentVariable("ASPNETCORE_LOGGER");
-            var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                if (loggerType == "NLog")
-                {
-                    builder.AddNLog("nlog.config");
-                }
-                else builder.AddConsole();
-            });
-            ILogger logger = loggerFactory.CreateLogger<Program>();
-
+            ILogger logger = LoggerProfile.GetLoggerFactory().CreateLogger<Program>();
             try
             {
                 logger.LogInformation("---------------------------------");
 
                 IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
                 hostBuilder.ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
 
-                if (loggerType == "NLog")
-                {
-                    hostBuilder.ConfigureLogging(logging => logging.ClearProviders());
-                    hostBuilder.UseNLog();
-                }
+                LoggerProfile.ConfigureLogging(hostBuilder);
 
                 IHost host = hostBuilder.Build();
+
                 Models.ApplicationDbSeed.Initialize(host);
+
                 host.Run();
             }
             catch (Exception exception)
@@ -48,11 +37,7 @@ namespace WebAPI
             }
             finally
             {
-                if (loggerType == "NLog")
-                {
-                    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                    NLog.LogManager.Shutdown();
-                }
+                LoggerProfile.Shutdown();
             }
         }
     }
---
+++ WebAPI/Setup/LoggerProfile.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+
+namespace WebAPI.Setup
+{
+    public static class LoggerProfile
+    {
+        public enum Kind
+        {
+            Unknown,
+            Console,
+            NLog,
+        }
+
+        private static Kind GetKind()
+        {
+            string value = Environment.GetEnvironmentVariable("ASPNETCORE_LOGGER");
+            Kind kind = Kind.Unknown;
+            Enum.TryParse<Kind>(value, out kind);
+            return kind;
+        }
+
+        public static ILoggerFactory GetLoggerFactory()
+        {
+            return LoggerFactory.Create(builder =>
+            {
+                if (GetKind() == Kind.NLog)
+                {
+                    builder.AddNLog("nlog.config");
+                }
+                else builder.AddConsole();
+            });
+        }
+
+        public static void ConfigureLogging(IHostBuilder hostBuilder)
+        {
+            if (GetKind() == Kind.NLog)
+            {
+                hostBuilder.ConfigureLogging(logging => logging.ClearProviders());
+                hostBuilder.UseNLog();
+            }
+        }
+
+        public static void Shutdown()
+        {
+            if (GetKind() == Kind.NLog)
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+    }
+}
