2eb9ad3b2446f1acf3839c32715fd9bc94744448
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-27 16:21:23 +0100

    Add firebase.js config file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRwGyf4UMLNO0R0m7Op2WYbQQG94_WcQc",
+    authDomain: "tests-712.firebaseapp.com",
+    projectId: "tests-712",
+    storageBucket: "tests-712.appspot.com",
+    messagingSenderId: "347722402306",
+    appId: "1:347722402306:web:7531be9ea5c4686921f51b"
+  };
