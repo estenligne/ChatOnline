e30de1147fcdffada54ca18964547f36011b8007
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-27 18:14:15 +0100

    Add DonutLarge, Chat and MoreVert icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
+
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 import './Sidebar.css';
 
 function Sidebar() {
@@ -7,6 +12,12 @@ function Sidebar() {
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar />
+
+                <div className="sidebar__headerRight">
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>
             </div>
 
             <div className="sidebar__search">
