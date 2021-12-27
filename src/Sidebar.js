751c6194572444acd18cfbc348ac2ec8ae8ae7fc
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-27 17:58:18 +0100

    Add the Avatar material-ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Avatar } from '@mui/material';
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar />
+            </div>
+
+            <div className="sidebar__search">
+                
+            </div>
+
+            <div className="sidebar__chats">
+                
+            </div>
         </div>
     )
 }
