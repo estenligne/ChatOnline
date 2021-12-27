6c9b2016fad37196460a8b45c81199b6b18c1b2c
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-27 17:09:37 +0100

    Added the Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
