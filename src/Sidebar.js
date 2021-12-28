dc195f9cf93a3654dd55aee90d16fa0c8eeaa754
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-28 18:05:23 +0100

    Implement the addNewChat adjusted component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -36,7 +36,7 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__chats">
-                <SidebarChat />
+                <SidebarChat addNewChat />
                 <SidebarChat />
                 <SidebarChat />
                 <SidebarChat />
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { Avatar } from '@mui/material';
 import './SidebarChat.css';
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
 
-    return (
+    const createChat = () => {};
+
+    return !addNewChat ? (
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2>Room name</h2>
                 <p>Last message...</p>
             </div>
+        </div>
+    ) : (
+        <div onClick={createChat} className="sidebarChat">
+            <h2>Add New Chat</h2>
         </div>
     )
 }
