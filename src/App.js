fd90e14656c675aac98644d4a4ccebd2efb6680f
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-28 18:27:17 +0100

    Add the Chat component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        {/* Chat */}
+        <Chat />
       </div>
     </div>
   );
---
+++ src/Chat.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Chat.css';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat__header">
+
+            </div>
+
+            <div className="chat__body">
+                
+            </div>
+
+            <div className="chat__footer">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
