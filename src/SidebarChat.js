9072cc26d9bdc65e3d7fce50141273dd4697a33a
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-28 17:28:48 +0100

    Implement the elements of SidebarChat, no styling

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Avatar } from '@mui/material';
 import './SidebarChat.css';
 
 function SidebarChat() {
     return (
         <div className="sidebarChat">
-            Hey
+            <Avatar />
+            <div className="sidebarChat__info">
+                <h2>Room name</h2>
+                <p>Last message...</p>
+            </div>
         </div>
     )
 }
