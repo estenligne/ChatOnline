759df0917b8d1673b7f538f99f9b0ff09d4410d2
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2021-12-28 18:34:30 +0100

    Add an avatar inside chat__header

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Avatar } from '@mui/material';
 import './Chat.css';
 
 function Chat() {
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
         <div className="chat">
             <div className="chat__header">
-
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             </div>
 
             <div className="chat__body">
