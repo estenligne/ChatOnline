229af71797ea4c77bc40ac9f21966013bfae4e76
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-03 17:30:19 +0100

    Fetch data from db in Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@mui/material';
 import { SearchOutlined } from '@mui/icons-material/';
 
@@ -7,9 +7,22 @@ import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import SidebarChat from './SidebarChat';
+import db from './firebase';
 import './Sidebar.css';
 
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot => (
+                setRooms(snapshot.docs.map(doc =>
+                    ({
+                        id: doc.id,
+                        data: doc.data(),
+                    })))
+            ));
+    }, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -37,9 +50,9 @@ function Sidebar() {
 
             <div className="sidebar__chats">
                 <SidebarChat addNewChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
+                {rooms.map(room => (
+                    <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+                ))}
             </div>
         </div>
     )
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Avatar } from '@mui/material';
 import './SidebarChat.css';
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
@@ -21,7 +21,7 @@ function SidebarChat({ addNewChat }) {
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
-                <h2>Room name</h2>
+                <h2>{name}</h2>
                 <p>Last message...</p>
             </div>
         </div>
