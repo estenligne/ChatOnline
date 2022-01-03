c2b758d4e4c3cc4353d35591a5d24273b3634cda
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-03 17:47:13 +0100

    Install and use react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route path="/rooms/:roomId" element={<Chat />} />
+            <Route path="/" element={<Chat />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
