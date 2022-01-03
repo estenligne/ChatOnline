9b2dc3c8606e9627da8f9ca43dbad20b0b828165
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-03 13:47:59 +0100

    Add div.chat__headerInfo and div.chat__chat__headerRight

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Avatar } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
+import { SearchOutlined, AttachFile, MoreVert } from '@mui/icons-material/';
 import './Chat.css';
 
 function Chat() {
@@ -13,6 +14,23 @@ function Chat() {
         <div className="chat">
             <div className="chat__header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                         <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="chat__body">
