d68e058ffe5dff79ab300425f750bdc6a97b2c4f
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-03 18:27:03 +0100

    Implement the Link react component

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
 import db from './firebase';
 import './SidebarChat.css';
 
@@ -22,13 +23,15 @@ function SidebarChat({ id, name, addNewChat }) {
     };
 
     return !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-            <div className="sidebarChat__info">
-                <h2>{name}</h2>
-                <p>Last message...</p>
+        <Link to={`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>Last message...</p>
+                </div>
             </div>
-        </div>
+        </Link>
     ) : (
         <div onClick={createChat} className="sidebarChat">
             <h2>Add New Chat</h2>
