668498ba5d5cbdd2276e76b1af4f82916a7a6f5e
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-11 15:17:59 +0100

    Added the Login component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Login from './Login';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     // BEM naming convention
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Routes>
-            <Route path="/rooms/:roomId" element={<Chat />} />
-            <Route path="/" element={<Chat />} />
-          </Routes>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Routes>
+              <Route path="/rooms/:roomId" element={<Chat />} />
+              <Route path="/" element={<Chat />} />
+            </Routes>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
