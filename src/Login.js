c9c8387ea3d090c809fde28294f971efe6686738
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-11 22:57:13 +0100

    Implement signIn() using GoogleAuthProvider

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Button } from '@mui/material/';
+import { auth, provider } from './firebase';
 import './Login.css';
 
 function Login() {
-    const signIn = () => {};
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then(result => console.log(result))
+            .catch(error => alert(error.message));
+    };
     
     return (
         <div className="login">
