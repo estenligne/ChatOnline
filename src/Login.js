7936dbf86ee7ad8096021306b00795513512d0c2
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-01-11 15:49:33 +0100

    Implement and style the Login component

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Button } from '@mui/material/';
 import './Login.css';
 
 function Login() {
+    const signIn = () => {};
+    
     return (
         <div className="login">
-            <h1>Login</h1>
+            <div className="login__container">
+                <img alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/langfr-330px-WhatsApp.svg.png" />
+
+                <div className="login__text">
+                    <h1>Sign in to WhatsApp</h1>
+                </div>
+
+                <Button type="submit" onClick={signIn}>
+                    Sign In With Google
+                </Button>
+            </div>
         </div>
     )
 }
