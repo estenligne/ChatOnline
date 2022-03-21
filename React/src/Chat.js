110cb445afc55acfb25ad51483e2dd06728f9fcc
spykelion <ndilionel6@gmail.com>
2022-03-21 13:11:25 +0100

    merge squash

--- React/src/Chat.js
+++ React/src/Chat.js
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, IconButton } from '@mui/material';
-import { SearchOutlined, AttachFile, MoreVert } from '@mui/icons-material/';
-import { InsertEmoticon, Mic } from '@mui/icons-material/';
-import { useParams } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
-import firebase from 'firebase/compat/app';
-import { _fetch, getFileURL, dateToLocal } from './global';
-import './Chat.css';
+import React, { useEffect, useState } from "react";
+import { Avatar, IconButton } from "@mui/material";
+import { SearchOutlined, AttachFile, MoreVert } from "@mui/icons-material/";
+import { InsertEmoticon, Mic } from "@mui/icons-material/";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import firebase from "firebase/compat/app";
+import { _fetch, getFileURL, dateToLocal } from "./global";
+import "./Chat.css";
+import { actionTypes } from "./reducer";
 
 function Chat() {
     const { roomId } = useParams();
     const [input, setInput] = useState("");
     const [roomInfo, setRoomInfo] = useState({});
-    const [messages, setMessages] = useState([]);
-    const [{ user }, dispatch] = useStateValue();
+    const [{ user, messages, rooms }, dispatch] = useStateValue();
+    const gotoLastMessageRef = React.useRef(null);
 
+    React.useEffect(() => {
+        gotoLastMessageRef.current.scrollIntoView({ behavior: "auto" });
+    });
     useEffect(() => {
         if (roomId) {
             _fetch(user, "/api/ChatRoom/GetInfo?id=" + roomId)
-                .then(response => response.json())
-                .then(roomInfo => setRoomInfo(roomInfo));
+                .then((response) => response.json())
+                .then((roomInfo) => setRoomInfo(roomInfo));
 
             _fetch(user, "/api/Message/GetMany?chatRoomId=" + roomId)
-                .then(response => response.json())
-                .then(messages => setMessages(messages));
+                .then((response) => response.json())
+                .then((message) => {
+                    dispatch({
+                        type: actionTypes.FETCH_MESSAGES,
+                        messages: message,
+                    });
+                });
         }
     }, [roomId, user]);
 
@@ -36,14 +45,31 @@ function Chat() {
             messageTag: { chatRoomId: roomId },
             body: input,
             dateSent: new Date().toJSON(),
-        }
+        };
 
         _fetch(user, "/api/Message", "POST", body)
-            .then(response => response.json())
-            .then(message => setMessages([...messages, message]));
+            .then((response) => response.json())
+            .then((message) => {
+                // update messages in the store
+                dispatch({
+                    type: actionTypes.FETCH_MESSAGES,
+                    messages: [...messages, message],
+                });
+                // update rooms in the store
+                _fetch(user, "/api/ChatRoom/GetAll")
+                    .then((response) => response.json())
+                    .then((_rooms) => {
+                        dispatch({
+                            type: actionTypes.SET_ROOMS,
+                            rooms: _rooms,
+                        });
+                        setRoomInfo(_rooms.find(room => room.id == parseInt(roomId)))
+                    });
+                });
 
+        console.log("after sent, ROOMS", rooms);
         setInput("");
-    }
+    };
 
     return (
         <div className="chat">
@@ -52,7 +78,13 @@ function Chat() {
 
                 <div className="chat__headerInfo">
                     <h3>{roomInfo.name}</h3>
-                    <p>{dateToLocal(roomInfo.latestMessage?.id ? roomInfo.latestMessage?.dateSent : null)}</p>
+                    <p>
+                        {dateToLocal(
+                            roomInfo.latestMessage?.id
+                                ? roomInfo.latestMessage?.dateSent
+                                : null
+                        )}
+                    </p>
                 </div>
 
                 <div className="chat__headerRight">
@@ -69,8 +101,14 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                {messages.map(message => (
-                    <p key={message.id} className={`chat__message ${message.senderId === roomInfo.userChatRoomId && "chat__receiver"}`}>
+                {messages.map((message) => (
+                    <p
+                        key={message.id}
+                        className={`chat__message ${
+                            message.senderId === roomInfo.userChatRoomId &&
+                            "chat__receiver"
+                        }`}
+                    >
                         <span className="chat__name">{message.senderName}</span>
                         {message.body}
                         <span className="chat__timestamp">
@@ -78,18 +116,26 @@ function Chat() {
                         </span>
                     </p>
                 ))}
+                <div ref={gotoLastMessageRef}></div>
             </div>
 
             <div className="chat__footer">
                 <InsertEmoticon />
                 <form>
-                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type a message" />
-                    <button onClick={sendMessage} type="submit">Send a message</button>
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        type="text"
+                        placeholder="Type a message"
+                    />
+                    <button onClick={sendMessage} type="submit">
+                        Send a message
+                    </button>
                 </form>
                 <Mic />
             </div>
         </div>
-    )
+    );
 }
 
 export default Chat;
