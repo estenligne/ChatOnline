233cb07fa1d98e6c245c0f26e6b276e260d71f1a
spykelion <ndilionel6@gmail.com>
2022-03-22 09:58:02 +0100

    Add notReceived and notRead Message count and update the UI approriately

--- React/src/SidebarChat.js
+++ React/src/SidebarChat.js
@@ -5,7 +5,20 @@ import { getFileURL } from './global';
 import './SidebarChat.css';
 
 function SidebarChat({ room, addNewChat }) {
+    
+     // this works as a checkmark. I don't have any idea about how it shows up here
+    const [checkmark, setCheckmark] = React.useState([''])
 
+    React.useEffect(()=>{
+        if(room){
+            if(room.latestMessage.notReadCount > 0){
+                setCheckmark((_prev)=>[..._prev, '✔'])
+            }
+            if(room.latestMessage.notReceivedCount > 0){
+                setCheckmark((_prev)=>[..._prev, '✔'])
+            }
+        }
+    },[])
     const createChat = () => {
         alert("Not yet implemented!");
     };
@@ -16,8 +29,15 @@ function SidebarChat({ room, addNewChat }) {
                 <Avatar src={getFileURL(room.photoFileName)} />
 
                 <div className="sidebarChat__info">
-                    <h2>{room.name}</h2>
-                    <p>{room.latestMessage.shortBody}</p>
+                    <div className='chat__messageInfo'>
+                        <h2>{room.name}</h2>
+                        <p>{room.latestMessage.shortBody}</p>
+                    </div>
+                    <div className='chat__readInfo'>
+                        {checkmark.map((mark, idx) => (
+                            <span key={idx}>{mark}</span>
+                        ))}
+                    </div>
                 </div>
             </div>
         </Link>
@@ -25,7 +45,7 @@ function SidebarChat({ room, addNewChat }) {
         <div onClick={createChat} className="sidebarChat">
             <h2>Add New Chat</h2>
         </div>
-    )
+    );
 }
 
 export default SidebarChat;
