9df9fd3d5c7ae050b1cb1ee2f6eb7e61a5a35070
Yemeli Tasse Cyrille <yemelitassecyrille@gmail.com>
2022-04-03 16:53:00 +0100

    enhance(react): remove carret and use OptionsButton

--- React/src/Message.js
+++ React/src/Message.js
@@ -1,105 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
+import { KeyboardArrowDown } from "@mui/icons-material";
 import { getFileURL, dateToLocal } from "./global";
+import OptionsButton from "./OptionsButton";
 import "./Message.css";
 
 function Message({ messages, message, roomInfo, setLinkedId }) {
+
+    const isSender = message.senderId === roomInfo.userChatRoomId;
+    const showName = !isSender && roomInfo.type === 2; // if a group
+
+    const options = [
+        { name: "Reply", callback: () => setLinkedId(message.id) },
+        { name: "Delete", callback: () => alert("Not yet implemented!") }
+    ];
+
+    const linked = messages.find(m => m.id === message.linkedId);
+
     return (
         <div
             key={message.id}
-            className={`chat__message ${message.senderId === roomInfo.userChatRoomId &&
-                "chat__receiver"
-                }`}
+            className={"chat__message" + (isSender ? " chat__sender" : "")}
         >
             <div className="chat__name">
-                <span>{message.senderName}</span>
-                <div className="chat_carretParent">
-                    <CarretDownIcon setLinkedId={setLinkedId} message={message} />
-                </div>
+                <span>{showName ? message.senderName : ""}</span>
+
+                <OptionsButton options={options}>
+                    <KeyboardArrowDown />
+                </OptionsButton>
             </div>
 
-            {message.linkedId ? (
-                <p className="chat__ref">
+            {linked ? (
+                <p className="chat__linked">
                     <span className="chat__refname">
-                        {
-                            getMessageById(
-                                messages,
-                                message.linkedId
-                            )?.senderName
-                        }
+                        {linked.senderName}
                     </span>
-                    {
-                        getMessageById(messages, message.linkedId)
-                            ?.body
-                    }
+                    {linked.body}
                 </p>
             ) : null}
 
             {message.file ? (
                 <div className="chat__image">
                     <div className="chat__imageLink">
-                        <img
-                            src={getFileURL(message.file.name)}
-                            alt=""
-                        />
+                        <img src={getFileURL(message.file.name)} alt="" />
                     </div>
                 </div>
             ) : null}
 
             {message.body}
+
             <span className="chat__timestamp">
                 {dateToLocal(new Date(message.dateSent))}
             </span>
         </div>
     );
-}
-
-function CarretDownIcon({ message, setLinkedId, ...props }) {
-    const [showChildren, setShowChildren] = useState(false)
-    return (
-        <div className="carret" {...props} onClick={() => setShowChildren(true)} onMouseLeave={() => setShowChildren(false)} >
-            <div className="">
-                <span
-                    data-testid="down-context"
-                    data-icon="down-context"
-                    className=""
-                >
-                    <svg viewBox="0 0 18 18" width="18" height="18">
-                        <path
-                            fill="currentColor"
-                            d="M3.3 4.6 9 10.3l5.7-5.7 1.6 1.6L9 13.4 1.7 6.2l1.6-1.6z"
-                        ></path>
-                    </svg>
-                </span>
-            </div>
-
-
-            <div className="message__options">
-                {showChildren ? (
-                    <div className="list__reply">
-                        <ul className="options">
-                            <li
-                                onClick={() => {
-                                    setLinkedId(
-                                        message.id
-                                    )
-                                    setShowChildren(false)
-                                }
-                                }
-                            >
-                                reply
-                            </li>
-                            <li>Delete</li>
-                        </ul>
-                    </div>
-                ) : ""}
-            </div>
-        </div>
-    );
-}
-
-function getMessageById(listOfMessages, id) {
-    const message = listOfMessages.filter(message => message.id === id)
-    return message[0]
 }
 
 export default Message;
